0daa8c82278d3b00b987aefe4924339f8bd8eb58
DavidHZL <hanzldavid707@gmail.com>
2022-01-30 10:36:22 -0600

    Functional RPS with UI

--- script.js
+++ script.js
@@ -2,35 +2,60 @@
 
 const rpsSelection = ["rock", "paper", "scissors"];
 
+var pScore = 0;
+var cScore = 0;
+
 function game() {
 
-    let i = 0;
+    // let i = 0;
 
-    do {
-        i += 1;
-        const playerSelection = playerPlay();
-        const computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-    } while (i < 5);
+    // do {
+    //     i += 1;
+    //     const playerSelection = playerPlay();
+    //     const computerSelection = computerPlay();
+    //     playRound(playerSelection, computerSelection);
+    // } while (i < 5);
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    
+    let computerSelection = computerPlay();
 
+    console.log(playerSelection);
+    console.log(computerSelection);
+    
     let result  = ""
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
         result = "Congrats you won!";
+        pScore += 1;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         result = "Congrats you won!";
+        pScore += 1;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         result = "Congrats you won!";
+        pScore += 1;
     } else if (playerSelection === computerSelection) {
         result = "It is a tie!";
     } else {
         result = "The Computer Won, You lose!";
+        cScore += 1;
     }
 
-    console.log(result + "\nPlayer: " + playerSelection + "\nComputer: "+ computerSelection);
+    let resultDiv = document.querySelector('.displayResults');
+    resultDiv.textContent = '';
+
+    let p = document.createElement('p');
+
+    p.textContent =  result + "\nPlayer: " + playerSelection + "\nComputer: "+ computerSelection;
+
+    let pScoreDiv = document.querySelector("#pScore");
+    let cScoreDiv = document.querySelector("#cScore");
+
+    pScoreDiv.textContent = pScore;
+    cScoreDiv.textContent = cScore;
+
+    resultDiv.append(p);
 }
 
 function playerPlay() {
@@ -49,5 +74,5 @@ function computerPlay() {
     return selection;
 }
 
-game();
+
 
